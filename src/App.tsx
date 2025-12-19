f8bf9ce8594291a1fbf2dd03c695e95f38802e45
sreekaratla81 <150553417+sreekaratla81@users.noreply.github.com>
2025-12-19 10:47:39 +0530

    Add support launcher chatbot with callback escalation

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,7 @@ import BlogHome from "./pages/blog/BlogHome"
 import BlogCategory from "./pages/blog/BlogCategory"
 import BlogPostPage from "./pages/blog/BlogPostPage"
 import ShortLinkRedirect from "./components/ShortLinkRedirect"
-import FloatingWhatsAppButton from "./components/support/FloatingWhatsAppButton"
-import HelpLauncher from "./components/support/HelpLauncher"
+import SupportLauncher from "./components/support/SupportLauncher"
 import SupportStickyBar from "./components/support/SupportStickyBar"
 
 function AppWrapper() {
@@ -60,8 +59,7 @@ function AppWrapper() {
         <Route path="/:shortCode" element={<ShortLinkRedirect />} />
         <Route path="/*" element={<PageNotFound />} />
       </Routes>
-      <HelpLauncher />
-      <FloatingWhatsAppButton />
+      <SupportLauncher />
       <SupportStickyBar />
       <Footer />
     </>
